ba50d707c37a97aaeaff3dde98bb5786c2c1d622
SergeyLebidko <it.sergeyler@mail.ru>
2021-01-20 14:49:06 +0300

    В модель Post добавлен метод __str__

--- api/models.py
+++ api/models.py
@@ -37,6 +37,9 @@ class Token(models.Model):
     token = models.CharField(max_length=32, verbose_name='Токен пользователя')
     account = models.ForeignKey(Account, verbose_name='Аккаунт', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.token
+
     class Meta:
         verbose_name = 'Токен пользователя'
         verbose_name_plural = 'Токены пользователей'
