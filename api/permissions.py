59a23222ff3f10ea01bb0ba7d13de0d71ccb3094
SergeyLebidko <it.sergeyler@mail.ru>
2021-01-28 18:47:23 +0300

    Фикс бага в permissions.py, препятствовавшего нормальному выполнению preflight-запросов

--- api/permissions.py
+++ api/permissions.py
@@ -4,4 +4,6 @@ from rest_framework.permissions import BasePermission
 class HasAccountPermission(BasePermission):
 
     def has_permission(self, request, view):
+        if request.method == 'OPTIONS':
+            return True
         return request.account is not None
