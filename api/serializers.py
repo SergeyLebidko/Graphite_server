c0d4f867da8879761073e4308c801a6abd18429e
SergeyLebidko <it.sergeyler@mail.ru>
2021-02-01 17:53:11 +0300

    Добавлен код сохранения файла с аватаром

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from utils import to_hash
+from utils import to_hash, create_random_string
 from .models import Account
 
 
@@ -15,6 +15,14 @@ class AccountSerializer(serializers.ModelSerializer):
         password = validated_data['password']
         validated_data['password'] = to_hash(password)
         return serializers.ModelSerializer.create(self, validated_data)
+
+    def update(self, instance, validated_data):
+        avatar = validated_data.get('avatar')
+        if avatar:
+            avatar.name = f'avatar_{instance.pk}_{create_random_string(5)}.jpg'
+            instance.avatar.delete()
+            instance.save()
+        return serializers.ModelSerializer.update(self, instance, validated_data)
 
     class Meta:
         model = Account
