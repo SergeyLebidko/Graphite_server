4a17a425c879fd6dcb65c7629671100264e49ca7
SergeyLebidko <it.sergeyler@mail.ru>
2021-02-18 17:18:08 +0300

    Реализован полнотекстовый поиск

--- api/full_text_search.py
+++ api/full_text_search.py
@@ -1,10 +1,72 @@
-from django.db.models import Q
+import re
+import itertools
+
+
+# Функции этого модуля реализуют полнотекстовый поиск среди постов и имен авторов или только среди имен авторов.
+# Управляет типом поиска (посты и имена авторов или только имена авторов) параметр queryset_type.
+# Сам поиск идет без учета регистра.
+# Строка поиска проходит ряд преобразований, влияющих на релевантность найденных значений.
+# Чем сильнее преобразована строка - тем менее релевантны найденные значения и тем дальше от первого места они будут
+# находиться в итоговом списке.
 
 
 def search(queryset, string, queryset_type):
-    if queryset_type == 'post':
-        queryset = queryset.filter(Q(text__contains=string) | Q(account__username__contains=string))
-    elif queryset_type == 'account':
-        queryset = queryset.filter(username__contains=string)
+    stages = [stage_1(string), stage_2(string), stage_3(string)]
+    stages.extend(stage_4(string))
 
-    return [] if queryset is None else list(queryset)
+    for e in stages:
+        print(e)
+
+    result = []
+    src_queryset = list(queryset)
+    for stage in stages:
+        result.extend(_search(src_queryset, result, stage, queryset_type))
+    return result
+
+
+def stage_1(string):
+    return [string]
+
+
+def stage_2(string):
+    return [string.strip()]
+
+
+def stage_3(string):
+    result = string.strip()
+    regex = re.compile(r'\s+')
+    result = regex.sub(' ', result)
+    return [result]
+
+
+def stage_4(string):
+    regex = re.compile(r'[^0-9a-zA-Zа-яА-я]+')
+    words = [e for e in regex.split(string) if e]
+    result = []
+    for count in range(len(words), 0, -1):
+        for combination in itertools.combinations(words, count):
+            result.append(combination)
+            clip = 1
+            while any([len(word) > 5 for word in result[-1]]):
+                result.append([(word if len(word) <= 5 else word[:-clip]) for word in result[-1]])
+                clip += 1
+
+    return result
+
+
+def _search(src_queryset, dst_queryset, strings, queryset_type):
+    result = []
+    for element in src_queryset:
+        find_count = 0
+        for string in strings:
+            string = string.lower()
+            if queryset_type == 'post':
+                find_count += (string in element.text.lower()) or (string in element.account.username.lower()) or (
+                        string in element.title.lower())
+            elif queryset_type == 'account':
+                find_count += string in element.username.lower()
+
+        if (find_count == len(strings)) and (element not in dst_queryset):
+            result.append(element)
+
+    return result
