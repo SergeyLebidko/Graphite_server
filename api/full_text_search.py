3b708ab7dbb0346aaa140c4964d4fe331abcd347
SergeyLebidko <it.sergeyler@mail.ru>
2021-02-18 13:14:42 +0300

    Исправлена опечатка в full_text_search.py

--- api/full_text_search.py
+++ api/full_text_search.py
@@ -5,6 +5,6 @@ def search(queryset, string, queryset_type):
     if queryset_type == 'post':
         queryset = queryset.filter(Q(text__contains=string) | Q(account__username__contains=string))
     elif queryset_type == 'account':
-        queryset = queryset.filte(username__contains=string)
+        queryset = queryset.filter(username__contains=string)
 
     return [] if queryset is None else list(queryset)
